013075f06e827d4ac429caa48a6090ea6f00bfbb
bilal-pucit <bcsf18a519@pucit.edu.pk>
2022-12-09 19:48:32 +0500

    Added Meals Summary and Available Meals Starting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals";
 
 function App() {
   return (
     <>
-      <Header/>
+        <Header/>
+        <Meals/>
     </>
   );
 }
